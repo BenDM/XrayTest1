c21bfd5187df071fdbf50014161225076ae8584a
Ben Douadia <simo2332001@yahoo.fr>
2018-06-25 14:47:41 +0200

    hh

---
+++ Features/Sample.feature
@@ -0,0 +1,6 @@
+Feature: Google Search
+
+  Scenario: Validate google search text field
+    Given I open a browser
+    And I navigate to google page
+    Then I validate the search tesxt field
---
+++ src/Test/StepDefinitions.java
@@ -0,0 +1,28 @@
+package Test;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class StepDefinitions {
+	
+	
+	@Given("^I open a browser$")
+	public void i_open_a_browser() throws Throwable {
+	    // Write code here that turns the phrase above into concretOe actions
+	    throw new PendingException();
+	}
+
+	@Given("^I navigate to google page$")
+	public void i_navigate_to_google_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I validate the search tesxt field$")
+	public void i_validate_the_search_tesxt_field() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
