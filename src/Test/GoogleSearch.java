2c2928e478f5ef08c94b202960cf9a9449bffc77
Ben Douadia <simo2332001@yahoo.fr>
2018-07-02 12:18:00 +0200

    qq

--- src/Test/GoogleSearch.java
+++ src/Test/GoogleSearch.java
@@ -23,23 +23,23 @@ public class GoogleSearch{
 //		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 //	}
 //
-//	@Given("^I open a browser$")
-//	public void i_open_a_browser() throws Throwable {
-//		// Thread.sleep(3000);
+	@Given("^I open a browser$")
+	public void i_open_a_browser() throws Throwable {
+		// Thread.sleep(3000);
 //		System.out.println("Current url : " + driver.getCurrentUrl());
-//	}
-//
-//	@When("^I navigate to google page$")
-//	public void i_navigate_to_google_page() throws Throwable {
+	}
+
+	@When("^I navigate to google page$")
+	public void i_navigate_to_google_page() throws Throwable {
 //		driver.get("http://www.google.com");
-//	}
-//
-//	@Then("^I validate the search tesxt field$")
-//	public void i_validate_the_search_tesxt_field() throws Throwable {
-//		// Assert.assertTrue(driver.findElement(By.name("q")).isDisplayed());
+	}
+
+	@Then("^I validate the search tesxt field$")
+	public void i_validate_the_search_tesxt_field() throws Throwable {
+		// Assert.assertTrue(driver.findElement(By.name("q")).isDisplayed());
 //		driver.findElement(By.name("q")).sendKeys("Cucumber");
-//		// Thread.sleep(1000);
-//	}
+		// Thread.sleep(1000);
+	}
 
 
 	///////////////////////////////////////////
